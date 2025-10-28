5005e01ba19e56361ab9b71646b4c37e2c120956
Hamza Rashid <hamzarashid@hamzas-air.myfiosgateway.com>
2025-10-28 16:41:30 -0400

    Initial commit for Spring Boot midterm project

---
+++ src/main/java/com/example/springboot_midterm/SpringbootMidtermApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_midterm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMidtermApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMidtermApplication.class, args);
+	}
+
+}
